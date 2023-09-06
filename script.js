03d4edf2d3bbf3b4947dba45a5fcd1c5db713382
Jose Ozuna <joseozuna48@gmail.com>
2023-09-06 14:08:25 -0700

    Implement game function that plays five rounds

--- script.js
+++ script.js
@@ -1,38 +1,67 @@
-const choices = ["rock","paper","scissors"];
+const choices = ["rock", "paper", "scissors"];
 
-function getComputerChoice(){
-    let selection = Math.floor(Math.random()*choices.length);
+function getComputerChoice() {
+    let selection = Math.floor(Math.random() * choices.length);
     return choices[selection];
 }
 
-function getPlayerSelection(){
+function getPlayerSelection() {
     let playerChoice = "";
-    
+
     playerChoice = playerChoice.toLowerCase();
 
-    while( !choices.includes(playerChoice)){
+    while (!choices.includes(playerChoice)) {
         playerChoice = prompt("Please select rock paper or scissors:");
         playerChoice = playerChoice.toLowerCase();
     };
 
     return playerChoice;
-    
+
 }
 
-function playRound(playerChoice,computerChoice){
-    let result = ""
-    if(playerChoice === computerChoice ){
-        result  = `It's a tie, you chose ${playerChoice} and the computer chose ${computerChoice}`;
-    }else if(playerChoice === "rock" && computerChoice === "paper"){
-        result = `You lose! ${computerChoice} beats ${playerChoice} `;
-    }else{
-        result = `You Win! ${playerChoice} beats ${computerChoice} `;
+function playRound(playerChoice, computerChoice) {
+    let resultString = ""
+    let result = 0;
+    if (playerChoice === computerChoice) {
+        resultString = `It's a tie, you chose ${playerChoice} and the computer chose ${computerChoice}`;
+    } else if ( (playerChoice === "rock" && computerChoice === "paper") || (playerChoice === "paper" && computerChoice === "scissors") || (playerChoice === "scissors" && computerChoice==="rock") ) {
+        resultString = `You lose! ${computerChoice} beats ${playerChoice} `;
+        result -= 1;
+    } else {
+        resultString = `You Win! ${playerChoice} beats ${computerChoice} `;
+        result += 1;
     }
+
+    console.log(resultString);
 
     return result;
 }
 
-let player = getPlayerSelection();
-let computer = getComputerChoice();
+function game() {
+    let player = "";
+    let computer = "";
 
-console.log( playRound(player,computer) );
+    let result = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        player = getPlayerSelection();
+        computer = getComputerChoice();
+        result = playRound(player, computer);
+
+        switch (result) {
+            case 1:
+                playerScore++;
+                break;
+            case -1:
+                computerScore++;
+                break;
+        }
+
+        console.log(`Player score: ${playerScore}, Computer Score: ${computerScore} Round ${i+1}`);
+
+    }
+}
+
+game();
